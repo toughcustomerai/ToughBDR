2ae6f4c21433c0492da32c35e9f2eb86d35cdc4f
toughcustomer <146129373+toughcustomer@users.noreply.github.com>
2023-11-10 15:06:02 -0800

    Update WelcomeForm.js
    hide all

--- app/components/WelcomeForm.js
+++ app/components/WelcomeForm.js
@@ -3,18 +3,16 @@ import { faFileUpload } from '@fortawesome/free-solid-svg-icons';
 import { LoadingCircle } from '../icons';
 
 const WelcomeForm = ({
-  assistantName,
-  setAssistantName,
-  assistantDescription,
-  setAssistantDescription,
-  assistantModel,
   setAssistantModel,
-  file,
-  handleFileChange,
+  assistantModel,
   startAssistant,
   isButtonDisabled,
   isStartLoading
 }) => {
+  // Hardcoded values
+  const assistantName = "ToughBDR";
+  const assistantDescription = "ToughBDR for ToughCustomer.ai";
+
   return (
     <div className="border-gray-500 bg-gray-200 sm:mx-0 mx-5 mt-20 max-w-screen-md rounded-md border-2 sm:w-full">
       <div className="flex flex-col space-y-4 p-7 sm:p-10">
@@ -22,24 +20,7 @@ const WelcomeForm = ({
           Welcome to Agent42!
         </h1>
         <form className="flex flex-col space-y-3">
-          <input
-            type="text"
-            placeholder="Assistant Name"
-            value={assistantName}
-            onChange={(e) => setAssistantName(e.target.value)}
-            required
-            className="p-2 border border-gray-200 rounded-md"
-          />
-
-          <input
-            type="text"
-            placeholder="Assistant Description"
-            value={assistantDescription}
-            onChange={(e) => setAssistantDescription(e.target.value)}
-            required
-            className="p-2 border border-gray-200 rounded-md"
-          />
-
+          {/* Model Selection Buttons */}
           <div>
             <button
               type="button"
@@ -57,37 +38,7 @@ const WelcomeForm = ({
             </button>
           </div>
 
-          <div
-            className="drop-area border-2 border-dashed border-gray-400 rounded-md p-4 text-center"
-            onClick={() => {
-              const fileInput = document.getElementById('file-input');
-              if (fileInput) {
-                fileInput.click();
-              }
-            }}
-          >
-            <input
-              id="file-input"
-              type="file"
-              accept=".c,.cpp,.csv,.docx,.html,.java,.json,.md,.pdf,.pptx,.txt,.tex,image/jpeg,image/png"
-              onChange={(e) => {
-                if (e.target.files) {
-                  handleFileChange(e.target.files[0]);
-                }
-              }}
-              required
-              style={{ display: 'none' }}
-            />
-            {file ? (
-              <>
-                <FontAwesomeIcon icon={faFileUpload} className="text-green-500 mb-2" />
-                <p className="text-gray-700 text-lg font-bold">{file.name}</p>
-              </>
-            ) : (
-              <p className="text-gray-500">Select a File</p>
-            )}
-          </div>
-
+          {/* Start Button */}
           <button
             type="button"
             onClick={startAssistant}
